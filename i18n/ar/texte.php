1df8f0482a1f30f724a4d03fa1c1eeff129776bc
Sonia Michaud <soso.michaud@icloud.com>
2022-10-05 17:01:40 -0400

    Langue arabe et citation en français si il n'en a pas dans la langue choisi

---
+++ i18n/ar/texte.php
@@ -0,0 +1,26 @@
+<?php  
+// Textes statiques du Site Web
+// EN
+
+/********** COMMUN ********************/
+// Tableau qui contient toute la méta information du site Web (par page)
+// Par exemple, le contenu de l'élément TITLE, et la valeur de l'attribut 
+// content de l'élément META (description)
+$meta = [
+    'accueil'   => [
+                    'h1'    =>  "Home",
+                    'titre' =>  "Home - Restaurant Leila", 
+                    'desc'  =>  "Restaurant Leila - Montreal"
+                ],
+    'menu'      => [
+                    'h1'    =>  "Menu",
+                    'titre' =>  "Menu - Restaurant Leila", 
+                    'desc'  =>  "Menu of Leila restaurant in Montreal. An eclectic and refined menu, combining creativity and tradition. Local products and Quebec specialties."
+                ],
+    'vins'      => [
+                    'h1'    =>  "Wine",
+                    'titre' =>  "Wine List - Restaurant Leila", 
+                    'desc'  =>  "Wine list of Leila restaurant in Montreal. Varied menu, including privately imported wines selected by our sommelier."
+                ]
+];
+?>
